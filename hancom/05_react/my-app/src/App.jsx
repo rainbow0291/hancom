7425e3ba53c2464aa9ec45ab2b17e76c973d7333
rainbow0291 <rainbow0291@gmail.com>
2026-07-13 16:58:30 +0900

    260713 수업자료정리
    React

--- hancom/05_react/my-app/src/App.jsx
+++ hancom/05_react/my-app/src/App.jsx
@@ -1,79 +1,35 @@
 import './App.css'
-// import Weather from './components/39/Weather'
-import Users from './components/38/Users'
-
-//import Counter from './components/36/Counter'
-// import Every from './components/37/Every'
-// import Clock from './components/35/Clock'
-//import Hello from './components/34/Hello'
-
-
-
-//import ProductItem from './components/33/ProductItem'
-//import NameForm from './components/32/NameForm.jsx'
-
-// import Counter from './components/31/Counter'
-//import Counter from './components/30/Counter'
-//import Counter from './components/29/Counter'
-//import Header from './components/28/Header'
-//import Menu from './components/28/Menu'
-//import Content from './components/28/Content'
-//import Footer from './components/28/Footer'
-
-//import SubmitButton from './components/27/SubmitButton'
-// import Tag from './components/26/Tag'
-// import Rating from './components/25/Rating'
-//import Alert from './components/24/Alert'
-// import Hello from './components/18/Hello.jsx'
-// import Greeting from './components/19/Greeting.jsx'
-//import Profile from './components/20/Profile.jsx' 
-//import Card from './components/21/Card.jsx'
-//import Avatar from './components/22/Avatar.jsx'
-//import Badge from './components/23/Badge.jsx' 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from "./pages/Home.jsx"
+import Catalog from "./pages/Catalog.jsx"
+import Game from "./pages/Game.jsx"
+import About from "./pages/About.jsx"
+import Contact from './pages/Contact.jsx'
 
 function App() {
-   {/*const list = ["HTML", "CSS", "JS", "REACT"]
-  const list2 = ["PYTHON", "JAVA", "C", "C#"]*/}
   return (
-    <>
-    {/* <SubmitButton/>*/}
-    {/*<Counter />*/}
-     {/*<ProductItem name="나이키 운동화" />*/}
-     {/*<Hello />*/}
-      {/*<Clock />*/}
-     {/*<Counter />*/}
-     {/* <Every />*/}
-     <Users />
-      {/*<Weather />*/}
-    {/*<NameForm />*/}
-     {/*<Header />
-    <Menu />
-    <Content/>  
-    <Footer/>  */}
-    {/*<Tag tags={list}/>
-     <Tag tags={list2}/>*/}
-     {/*<Rating score={4}/>
-    <Rating score={3}/>
-    <Rating score={2}/>*/}
+    <BrowserRouter>
+      <nav className="main-nav">
+        <div className="nav-container">
+          <Link to="/" className="nav-logo">📚 Book Store</Link>
+          <ul className="nav-menu">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/catalog">Catalog</Link></li>
+            <li><Link to="/game">Game</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </nav>
 
-     {/*<Alert type = "success" text="성공했습니다."/>
-    <Alert type = "error" text="실패했습니다."/>
-    <Alert type = "warning" text="주의하세요."/>
-    <Alert type = "origin" text="동작중입니다."/> */}
-      {/*}
-      <h1>삼항 연산자 조건식 ? True : False </h1>
-      <Badge text="안녕하세요, 이것은 True입니다." type="new"/>
-      <Badge text="안녕하세요, 이것은 False입니다." type="not"/> */}
-
-      {/*<Avatar name="정유진" online={true}/>*/}
-      {/*<Avatar name="이은영" online={false}/>*/}
-
-    {/* <Hello/> */}
-    {/* <Greeting name="React" age="20"/>*/}
-    {/*<Profile name="정유진" job="웹 프론트"/>*/}
-    {/*<Card title="React 고수되기" desc="기초부터 차근차근" emoji="⭐"/>*/}
-     {/* 여기 안에 있는 코드 모두 삭제 */}
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/catalog" element={<Catalog/>}/>
+        <Route path="/game" element={<Game/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
